bd1bde215bdb7f50212cb8c2196aba5a24c65261
Christophe Jollivet <jollivet@apside.fr>
2022-10-25 17:28:59 +0200

    introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'crm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularCRM';
+  title:string = 'AngularCRM';
+  maClass:string = 'textRed';
+  message:string = 'C\'est cool';
+  exist:boolean =false;
+  fruits:string[]= ['apple', 'pear','cherry'];
+
+  changeMessage($event:MouseEvent):void{
+    console.log($event);
+    this.message= 'ca marche';
+    this.maClass='textYellow'
+    this.exist=!this.exist;
+  }
 }
